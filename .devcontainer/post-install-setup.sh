dde8dc0ea3f9c1dd1500293b7db9c67ebca5203c
Michael Osborne <resonatortune@gmail.com>
2024-11-20 23:22:20 -0800

    commit flake to run devcontainer

---
+++ .devcontainer/post-install-setup.sh
@@ -0,0 +1,17 @@
+# Use official oh-my-zsh installer
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Add Znap for zsh dependencies
+echo '[[ -r ~/Repos/znap/znap.zsh ]] || git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git ~/Repos/znap' >> ~/.zshrc && \
+    echo 'source ~/Repos/znap/znap.zsh' >> ~/.zshrc
+
+# Add zsh plugins
+echo 'znap source marlonrichert/zsh-autocomplete' >> ~/.zshrc && \
+    echo 'znap source zsh-users/zsh-autosuggestions' >> ~/.zshrc && \
+    echo 'znap source zsh-users/zsh-syntax-highlighting' >> ~/.zshrc
+
+# Add direnv to bash
+eval "$(direnv hook bash)" >> ~/.bashrc
+
+# load zsh if bash loads
+echo 'exec zsh' >> ~/.bashrc
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A basic flake supporting linux and mac";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+      systems = [ "aarch64-linux" "x86_64-linux" "x86_64-darwin" ];
+      devShellForSystem = system: let
+        pkgs = import nixpkgs { 
+          inherit system; 
+          config.allowUnfree = true; # Needed for terraform and other "unfree" packages
+        };
+      in pkgs.mkShell {
+        buildInputs = with pkgs; [
+          zsh
+          # nodejs_20
+          # htop
+          # jdk8
+          # python3
+          # docker
+          # azure-cli
+          # terraform
+          # dotnet-sdk_8
+          # neo-cowsay
+          # gh
+        ];
+      };
+    in
+    {
+      devShells = lib.listToAttrs (map (system: {
+        name = system;
+        value = {
+          default = devShellForSystem system;
+        };
+      }) systems);
+    };
+}
