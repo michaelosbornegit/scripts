238219dc793a7e533a9d8bc5cce313f408126c66
Mike Osborne <mosborne@westmonroe.com>
2024-11-20 22:51:59 -0800

    add personalized devcontainer with my preferred extensions

--- docker/dev-container/minimal-nix-vs-code-devcontainer/flake.nix
+++ docker/dev-container/minimal-nix-vs-code-devcontainer/flake.nix
@@ -8,25 +8,25 @@
   outputs = { self, nixpkgs, ... }:
     let
       lib = nixpkgs.lib;
-      systems = [ "aarch64-linux" "x86_64-linux" ];
+      systems = [ "aarch64-linux" "x86_64-linux" "x86_64-darwin" ];
       devShellForSystem = system: let
         pkgs = import nixpkgs { 
           inherit system; 
-          # config.allowUnfree = true; # Needed for terraform
+          config.allowUnfree = true; # Needed for terraform and other "unfree" packages
         };
       in pkgs.mkShell {
         buildInputs = with pkgs; [
           zsh
-          nodejs_20
+          # nodejs_20
           # htop
           # jdk8
           # python3
           # docker
-          azure-cli
+          # azure-cli
           # terraform
           # dotnet-sdk_8
           # neo-cowsay
-          gh
+          # gh
         ];
       };
     in
